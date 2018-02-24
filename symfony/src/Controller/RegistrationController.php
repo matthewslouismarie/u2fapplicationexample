cdb164753d2b06fe9bcb6ea8f182aaaf6c54ba7f
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-02-24 16:13:36 +0000

    Make the registration work

--- symfony/src/Controller/RegistrationController.php
+++ symfony/src/Controller/RegistrationController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Member;
 use App\Form\RegistrationForm;
+use App\Repository\MemberRepository;
 use App\Submission\RegistrationSubmission;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
@@ -15,13 +18,19 @@ class RegistrationController extends AbstractController
      *  "/register",
      *  name="registration")
      */
-    public function register()
+    public function register(MemberRepository $mr, Request $request)
     {
         $submission = new RegistrationSubmission();
         $form = $this
             ->createForm(RegistrationForm::class, $submission)
             ->add('submit', SubmitType::class)
         ;
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $member = new Member(null, $submission->getUsername());
+            $mr->save($member, $submission->getPassword());
+        }
 
         return $this->render('registration.html.twig', [
             'form' => $form->createView(),
--- symfony/src/Repository/MemberRepository.php
+++ symfony/src/Repository/MemberRepository.php
@@ -5,11 +5,28 @@ namespace App\Repository;
 use App\Entity\Member;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class MemberRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    private $encoder;
+
+    public function __construct(
+        RegistryInterface $registry,
+        UserPasswordEncoderInterface $encoder)
     {
         parent::__construct($registry, Member::class);
+        $this->encoder = $encoder;
+    }
+
+    public function save(Member $member, string $password)
+    {
+        $member->setPassword($this
+            ->encoder
+            ->encodePassword($member, $password)
+        );
+        $em = $this->getEntityManager();
+        $em->persist($member);
+        $em->flush();
     }
 }
