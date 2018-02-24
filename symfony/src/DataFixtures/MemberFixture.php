a7835f91b3ccdef6a1ccf110b7111f9365c41041
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-02-24 14:36:01 +0000

    Add fixture for Member class

---
+++ symfony/src/DataFixtures/MemberFixture.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Member;
+use App\Entity\U2fToken;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class MemberFixture extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $member = new Member(null, 'louis');
+        $encoded = $this->encoder->encodePassword($member, 'hello');
+        $member->setPassword($encoded);
+        $manager->persist($member);
+
+        $manager->flush();
+    }
+}
