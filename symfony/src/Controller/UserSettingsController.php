5c7d0d84ed70e81252c49a9917766dff06279290
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-02-24 16:35:01 +0000

    Allow user to change their password

--- symfony/src/Controller/UserSettingsController.php
+++ symfony/src/Controller/UserSettingsController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Form\PasswordForm;
 use App\Submission\PasswordSubmission;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserSettingsController extends AbstractController
 {
@@ -15,13 +18,29 @@ class UserSettingsController extends AbstractController
      *  "/settings",
      *  name="settings")
      */
-    public function settings()
+    public function settings(
+        EntityManagerInterface $em,
+        Request $request,
+        UserPasswordEncoderInterface $encoder)
     {
         $submission = new PasswordSubmission();
         $form = $this
             ->createForm(PasswordForm::class, $submission)
             ->add('submit', SubmitType::class)
         ;
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $member = $this->getUser();
+            var_dump($member);
+            var_dump($submission->getPassword());
+            $newPassword = $encoder
+                ->encodePassword($member, $submission->getPassword())
+            ;
+            $member->setPassword($newPassword);
+            $em->persist($member);
+            $em->flush();
+        }
 
         return $this->render('settings.html.twig', [
             'form' => $form->createView(),
